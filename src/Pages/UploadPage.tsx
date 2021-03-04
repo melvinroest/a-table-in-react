554f36ac36d654a0268991adbe20ab631be9881e
melvin roest <melvinroest@gmail.com>
2021-03-04 16:31:25 +0100

    implemented basic UI for upload

--- src/Pages/UploadPage.tsx
+++ src/Pages/UploadPage.tsx
@@ -1,17 +1,56 @@
 import styles from "./UploadPage.module.scss";
+import React, { useCallback } from 'react'
+
 import { Upload } from 'react-feather';
+import { FileWithPath, useDropzone } from 'react-dropzone'
+import classnames from 'classnames'; 
 
 function UploadPage() {
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file: FileWithPath) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        console.log(binaryStr)
+      }
+      reader.readAsArrayBuffer(file)
+    })
+    
+  }, [])
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject, acceptedFiles } = useDropzone({ onDrop });
+  const files = acceptedFiles.map((file: FileWithPath) => <li key={file.path}>{file.path}</li>);
+  const allClassNames = classnames('dropzone', styles.container, { [styles.activeStyle]: isDragActive }, { [styles.acceptStyle]: isDragAccept }, { [styles.rejectStyle]: isDragReject });
+
   return (
-    <div id="UploadComponent">
-      <div className={styles.uploadCenter} >
+   <section>
+      <div {...getRootProps({className: allClassNames})}>
+        <input {...getInputProps()} />
         <Upload color="black" size={128} />
+        <p>Upload CSV</p>
       </div>
-      <p className={styles.uploadCenter}>
-        Upload CSV
-      </p>
-    </div>
-  );
-} 
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  )
+}
+
+// function UploadPage() {
+//   return (
+//     <div id="UploadComponent">
+//       <div className={styles.uploadCenter} >
+//         <Upload color="black" size={128} />
+//       </div>
+//       <p className={styles.uploadCenter}>
+//         Upload CSV
+//       </p>
+//     </div>
+//   );
+// } 
 
 export default UploadPage;
