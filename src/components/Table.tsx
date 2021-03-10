d08126e38ac79787f9966ff5f4936b0248c26ff3
melvin roest <melvinroest@gmail.com>
2021-03-10 21:00:47 +0100

    integrated upload functionality with backend

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useTable, useSortBy, useFilters, useAsyncDebounce, useRowSelect } from 'react-table';
-import { matchSorter } from 'match-sorter';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
-import { lighten, makeStyles } from '@material-ui/core/styles';
+import MaUTable from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx'
 
 const useToolbarStyles = makeStyles((theme: any) => ({
@@ -41,132 +46,8 @@ const IndeterminateCheckbox = React.forwardRef(
   }
 )
 
-// Define a default UI for filtering
-function DefaultColumnFilter({
-  column: { filterValue, preFilteredRows, setFilter },
-}: any) {
-  const count = preFilteredRows.length
-
-  return (
-    <input
-      value={filterValue || ''}
-      onChange={e => {
-        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-      }}
-      placeholder={`Search ${count} records...`}
-    />
-  )
-}
-
-// This is a custom UI for our 'between' or number range
-// filter. It uses two number boxes and filters rows to
-// ones that have values between the two
-function NumberRangeColumnFilter({
-  column: { filterValue = [], preFilteredRows, setFilter, id },
-}: any) {
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    preFilteredRows.forEach((row: any) => {
-      min = Math.min(row.values[id], min)
-      max = Math.max(row.values[id], max)
-    })
-    return [min, max]
-  }, [id, preFilteredRows])
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-      }}
-    >
-      <input
-        value={filterValue[0] || ''}
-        type="number"
-        onChange={e => {
-          const val = e.target.value
-          setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
-        }}
-        placeholder={`Min (${min})`}
-        style={{
-          width: '70px',
-          marginRight: '0.5rem',
-        }}
-      />
-      to
-      <input
-        value={filterValue[1] || ''}
-        type="number"
-        onChange={e => {
-          const val = e.target.value
-          setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
-        }}
-        placeholder={`Max (${max})`}
-        style={{
-          width: '70px',
-          marginLeft: '0.5rem',
-        }}
-      />
-    </div>
-  )
-}
-
-function fuzzyTextFilterFn(rows: any, id: any, filterValue: any) {
-  return matchSorter(rows, filterValue, { keys: [ (row: any) => row.values[id]] })
-}
-
 function Table({ columns, data, setData }: any) {
   const classes = useToolbarStyles();
-  const filterTypes = React.useMemo(
-    () => ({
-      // Add a new fuzzyTextFilterFn filter type.
-      fuzzyText: fuzzyTextFilterFn,
-      // Or, override the default text filter to use
-      // "startWith"
-      text: (rows: any, id: any, filterValue: any) => {
-        return rows.filter( (row: any) => {
-          const rowValue = row.values[id]
-          return rowValue !== undefined
-            ? String(rowValue)
-                .toLowerCase()
-                .startsWith(String(filterValue).toLowerCase())
-            : true
-        })
-      },
-    }),
-    []
-  )
-
-  const defaultColumn = React.useMemo(
-    () => ({
-      // Let's set up our default Filter UI
-      Filter: DefaultColumnFilter,
-    }),
-    []
-  )
-
-  // Need to use React.UseMemo or Memoizee
-  columns = React.useMemo(() => {
-    const result = columns.reduce((accumulator: any, header: any, index: number) =>
-      {
-        let filterUserInterface = DefaultColumnFilter;
-        let filterFunction = "equals";
-        if (header?.type === "number") {
-          filterUserInterface = NumberRangeColumnFilter;
-          filterFunction = "between";
-        }
-        header = {
-          ...header,
-          Filter: filterUserInterface,
-          filter: filterFunction
-        }
-        return [
-          ...accumulator,
-          header
-        ]
-      }, [])
-    return result;
-  }, []);
 
   // Use the state and functions returned from useTable to build your UI
   const {
@@ -180,8 +61,6 @@ function Table({ columns, data, setData }: any) {
     {
       columns,
       data,
-      defaultColumn, // Be sure to pass the defaultColumn option
-      filterTypes,
     },
     useFilters, // useFilters!
     useSortBy,
@@ -191,7 +70,6 @@ function Table({ columns, data, setData }: any) {
         // Let's make a column for selection
         {
           id: 'selection',
-          Header: ({ getToggleAllRowsSelectedProps }) => (null),
           // The cell can use the individual row's getToggleRowSelectedProps method
           // to the render a checkbox
           Cell: ({ row }: any) => (
@@ -210,19 +88,18 @@ function Table({ columns, data, setData }: any) {
 
   const deleteUserHandler = (event: any) => {
     const newData = removeByIndexs(data, Object.keys(selectedRowIds).map(x => parseInt(x, 10)));
-    console.log(newData);
     setData(newData);
   }
   const numSelected = Object.keys(selectedRowIds).length;
 
   // Render the UI for your table
   return (
-    <>
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
+    <TableContainer>
+      <Toolbar
+        className={clsx(classes.root, {
+          [classes.highlight]: numSelected > 0,
+        })}
+      >
       {numSelected > 0 ? (
         <Typography
           className={classes.title}
@@ -241,42 +118,32 @@ function Table({ columns, data, setData }: any) {
         </Tooltip>
       ) : null}
     </Toolbar>
-      <table {...getTableProps()}>
-        <thead>
+      <MaUTable {...getTableProps()}>
+        <TableHead>
           {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                <TableCell {...column.getHeaderProps()}>
                   {column.render('Header')}
-                  {/* Add a sort direction indicator */}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                  {/* Render the columns filter UI */}
-                  <div>{column.canFilter ? column.render('Filter') : null}</div>
-                </th>
+                </TableCell>
               ))}
-            </tr>
+            </TableRow>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
           {rows.map((row: any, i: number) => {
             prepareRow(row)
             return (
-              <tr {...row.getRowProps()}>
+              <TableRow {...row.getRowProps()}>
                 {row.cells.map((cell: any) => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  return <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>
                 })}
-              </tr>
+              </TableRow>
             )
           })}
-        </tbody>
-      </table>
-    </>
+        </TableBody>
+      </MaUTable>
+    </TableContainer>
   )
 }
 
