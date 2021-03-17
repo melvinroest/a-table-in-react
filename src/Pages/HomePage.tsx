b4552c499dbce09a1bcb47d77fe0340bd128df99
melvin roest <melvinroest@gmail.com>
2021-03-17 21:51:30 +0100

    final version for challenge

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,8 +1,11 @@
-
 function HomePage() {
   return (
     <div id="HomeComponent">
-      Home
+      <h1>Home</h1>
+      <h2>A how to for marketeers</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/SEH7wuzX9lo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <h2>A reflection video (for the devs assessing this challenge)</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/I5UExuMkPuU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   );
 } 
